26985ee230b9317431cd1bb3700b146f78298c38
Sudhansu Panda <sudhansupandaaa@gmail.com>
2021-09-24 00:06:53 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("cash-given");
+const checkButton = document.querySelector("chck-btn");
+const  resetButton = document.querySelector("rst-button");
+const message = document.querySelector("error-message");
+const noOfNotes = document.querySelector("no-of-notes");
+

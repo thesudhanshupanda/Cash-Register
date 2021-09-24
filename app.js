f8b2599e00081f934266ef4e5c7b159fcd017b14
Sudhansu Panda <sudhansupandaaa@gmail.com>
2021-09-25 00:35:41 +0530

    final

--- app.js
+++ app.js
@@ -3,15 +3,17 @@ const cashGiven = document.querySelector("#cashgiven");
 const checkButton = document.querySelector("#chck-btn");
 const  resetButton = document.querySelector("#rst-button");
 const message = document.querySelector("#error-message");
-const cashTable =document.querySelector(".cashtable");
+const cash =document.querySelector("#cashtable");
 const noOfNotes = document.querySelectorAll(".no-of-notes");
+const rstButton = document.querySelector("#rst-btn");
 
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
-// cashTable.style.display ="none";
+// cash.style.display = "none";
 
 checkButton.addEventListener("click", cashamountValidate);
 
+rstButton.addEventListener("click", reset);
 
 function cashamountValidate(){
  message.style.display = "none";
@@ -22,39 +24,35 @@ function cashamountValidate(){
      if(cashGivenValue > billAmountValue){ 
          const moneyToReturn = cashGivenValue - billAmountValue;
          calculateChange(moneyToReturn);
-         cashTable.style.display ="block";
-
+        //  cashTable.style.display ="block";
      }else if(cashGivenValue === billAmountValue){
-        console.log("both are equal"); 
          showMessage("The cash given is equal to bill amount, hence no return money");
 
      }else {
-        console.log("atlest equal to bill amount");
          showMessage("The cash given should be atleast equal to bill amount");
-
+        //  cashTable.style.display ="none";
      }
-
  }else{
-
     showMessage("Invalid bill amount")
+    // cashTable.style.display ="none";
  }
 }
 
 function calculateChange(moneyToReturn) {
 
  for ( let i=0 ; i < availableNotes.length ; i++) {
-
      const numOfNotes = Math.trunc(moneyToReturn / availableNotes[i]) ;
      moneyToReturn = moneyToReturn % availableNotes[i];
      noOfNotes[i].innerText = numOfNotes ;
-
-
  }
 }
 
 function showMessage(msg){
-    console.log("here");
     message.style.display ="block";
     message.innerText = msg;
 
 }
+
+function reset() {
+    location.reload();
+ }
